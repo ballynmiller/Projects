b8515b436926e9c18f6890467871f30cedcf09c4
Ballyn Miller <ballyn.miller@gmail.com>
2014-12-30 23:37:31 -0500

    Adding Capistrano files

---
+++ django_apps/config/deploy.rb
@@ -0,0 +1,94 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'websites'
+set :repo_url, 'git@github.com:ballynmiller/Sites.git'
+set :branch, :master
+set :deploy_to, "/var/www/#{fetch(:application)}"
+set :nginx_path, "/etc/nginx/sites-enabled/"
+
+namespace :deploy do
+	task :create_configs do
+		on roles(:all) do
+			execute "ln -s #{current_path}/django_apps/etc #{shared_path}/etc"
+			execute "cp #{shared_path}/etc/jakore #{fetch(:nginx_path)}/jakore"
+		end
+	end
+
+	task :create_virtual_env do
+		on roles(:all) do
+			execute "virtualenv #{current_path}/django_apps/websites/.venv"
+		end
+	end
+end
+
+before :deploy, :kill_nginx do
+	on roles(:all) do
+		if test("[ -f /tmp/run/jakore.pid ]")
+			execute "kill -9 $(cat /tmp/run/jakore.pid)"
+		end
+	end
+end
+
+before :deploy, :cleanup_files do
+	on roles(:all) do
+		if test("[ -d #{shared_path} ]")
+			execute "rm -rf #{shared_path}"
+		end
+
+		if test("[ -d /tmp/run ]")
+			execute "rm -rf /tmp/run"
+		end
+	end
+end
+
+after :deploy, :start_nginx do
+	on roles(:all) do
+		invoke "deploy:create_configs"
+		invoke "deploy:create_virtual_env"
+		execute ". #{current_path}/django_apps/websites/.venv/bin/activate"
+		execute "pip install -Ur #{current_path}/django_apps/requirements.txt"
+		invoke "maintenance:start"
+	end
+end
+
+namespace :maintenance do
+	desc "Starts nginx server for application"
+	task :start do
+		on roles(:web) do
+			if !Dir.exists?("/tmp/run")
+				execute "mkdir /tmp/run"
+			end
+			
+			# update settings file
+			execute "sed -i 's/{PASSWORD}/PASSWORD/g' #{current_path}/django_apps/websites/jakore_settings.py"
+
+			#update jakore file
+			execute "sed -i 's/{ASSETS_PATH}/\\/var\\/www\\/websites\\/current\\/django_apps\\/media/g' /etc/nginx/sites-enabled/jakore"
+
+			# update uwsgi file
+			execute "sed -i 's/{PATH}/\\/var\\/www\\/websites\\/current\\/django_apps/g' #{shared_path}/etc/uwsgi.ini"
+			execute "uwsgi --ini #{shared_path}/etc/uwsgi.ini"
+
+			# reload nginx
+			execute "service nginx reload"
+		end
+	end
+
+	desc "Kills nginx server for application"
+	task :stop do
+		on roles(:web) do
+			if test("[ -f /var/run/jakore.pid ]")
+				execute "kill -9 $(cat /var/run/jakore.pid)"
+			end
+		end
+	end
+
+	desc "Restarts nginx for application"
+	task :restart do
+		on roles(:web) do
+			invoke "maintenance:stop"
+			invoke "maintenance:start"
+		end
+	end
+end
